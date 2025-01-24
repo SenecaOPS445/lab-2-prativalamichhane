3e1e752498f6e2ddf1e5b4301f7261958353b7ce
Prativa Lamichhane <plamichhane2@myseneca.ca>
2025-01-24 12:15:47 -0500

    lab2f.

---
+++ lab2f.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#Author: Prativa Lamichhane
+#Author ID: 167520220
+#Date Created: 2025/01/24
+import sys
+timer = int(sys.argv[1])
+
+while timer > 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
+
